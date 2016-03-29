5eaaa436d89b8ee19273fd87236a383116aa3262
murashin <ssmuramatsu@gmail.com>
2016-03-30 00:14:17 +0900

    initial commit

---
+++ tweet_delete.py
@@ -0,0 +1,36 @@
+from requests_oauthlib import OAuth1Session
+import configparser
+import os
+import sys
+import json
+
+config = config.ConfigParser()
+config.read("config.ini")
+token = config["token"]
+
+CK = token["CK"]                # Consumer Key
+CS = token["CS"]                # Consumer Secret
+AT = token["AT"]                # Access Token
+AS = token["AS"]                # Access Token Secret
+
+DEL_NUM = 120                   # 一度に消すツイートの数
+
+with open("tweet_list.js") as f:
+    twi_list = json.load(f)
+
+start = 0
+if os.path.exists("./cnt.txt"):
+    with open("cnt.txt", "r") as f:
+        start = int(f.read().rstrip())
+
+twitter = OAuth1Session(CK, CS, AT, AS)
+del_url = "https://api.twitter.com/1.1/statuses/destroy/{}.json"
+cnt = 0
+for i in range(start, start+TWI_NUM):
+    twitter.post(del_url.format(twi_list[i]["id"]))
+    cnt += 1
+
+print("全部で{}件ツイートを削除しました。".format(cnt))
+
+with open("cnt.txt", "w") as f:
+    f.write(str(start+TWI_NUM))
